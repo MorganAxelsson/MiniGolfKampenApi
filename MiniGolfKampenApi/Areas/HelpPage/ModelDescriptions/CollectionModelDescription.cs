3e5c3bf8e1807dc69d08a2f150a04af7fed81e07
Morgan Axelsson <morgan.axelsson@osolo.se>
2018-05-02 09:13:59 +0200

    Add project files.

---
+++ MiniGolfKampenApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MiniGolfKampenApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MiniGolfKampenApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MiniGolfKampenApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
